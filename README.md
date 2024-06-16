67b9a8ed00778b7e073fc31c98d5bbdcb5e3eb35
Rafaela <rafinha-2410@hotmail.com>
2024-06-16 01:07:31 -0300

    Alteração em em melhoria do codigo

---
+++ README.md
@@ -0,0 +1,1 @@
+Consiste em criar um programa que leia a idade de uma pessoa expressa em anos, meses e dias e mostre-a expressa em dias.
--- src/IdadeEmDias.java
+++ src/IdadeEmDias.java
@@ -1,15 +1,27 @@
+
+import java.util.Scanner;
+
 public class IdadeEmDias {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); 
         
         // leitura da idade em anos, meses e dias
-        int anos = 26;
-        int meses = 8;
-        int dias = 23;
+        System.out.println("Digite a quaantidade de anos: ");
+        int anos = scanner.nextInt();
+
+        System.out.println("Digite a quantidade de meses: ");
+        int meses = scanner.nextInt();
+
+        System.out.println("Digite a quantidade de dias: ");
+        int dias = scanner.nextInt();
+
+        scanner.close();
 
         // calculo da idade em dias
         int idadeEmDias = anos * 365 + meses * 30 + dias;
 
         // Exibição do resultado
         System.out.println("A idade de " + anos + " anos, " + meses + " meses e " +  dias + " dias é igual a " + idadeEmDias + " dias. ");
+    
     }
 }
